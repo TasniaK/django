93893397a73fbad0f15166ed8df1f8e54b558d66
Tasnia Kamal <tasniakamal@tasnias-mbp.home>
2018-03-09 17:23:32 +0000

    Additional templates details and results added and views modified for results and votes

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+#once models are made, they need to be registered to view them on the admin page of the website
 from .models import Question
 
 admin.site.register(Question)
--- polls/urls.py
+++ polls/urls.py
@@ -2,15 +2,17 @@ from django.conf.urls import url
 
 from . import views
 
-#regex or regular expressions, used to dynamically scrape pages for information you're looking for
-#r'' contains the expressions used to filter through data
-#^ matches start of a string
-#?P<name> in parentheses allows this whole group to then have other regexs applied after them, by allowing the returned value from the group to be available to the view function
-#[0-9] looks for a digit between 0 and 9 
-#+ looks for one or more occurences, in this case one or more occurences of a digit between 0 and 9
-#/ is not a special character in regex so this is simply looking for a /
-#$ matches the end of a string
+# regex or regular expressions, used to dynamically scrape pages for information you're looking for
+# r'' contains the expressions used to filter through data
+# ^ matches start of a string
+# ?P<name> in parentheses allows this whole group to then have other regexs applied after them
+# [0-9] looks for a digit between 0 and 9
+# + looks for one or more occurences, in this case one or more occurences of a digit between 0 and 9
+# / is not a special character in regex so this is simply looking for a /
+# $ matches the end of a string
 # next input is what variable the regex is looking in
+
+app_name = 'polls'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
